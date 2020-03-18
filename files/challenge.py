bd5d81dfd7060140cc54ab59436af1dd9b123e46
gpadolina <47703315+gpadolina@users.noreply.github.com>
2020-03-17 21:19:36 -0700

    Update challenge.py

--- files/challenge.py
+++ files/challenge.py
@@ -3,3 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 %matplotlib inline
 plt.style.use('classic')
+
+pageviews = pd.read_csv("/Users/--/Desktop/January 2019 pageviews.csv",
+                        skiprows=5, sep=',', thousands=',', nrows=31)
